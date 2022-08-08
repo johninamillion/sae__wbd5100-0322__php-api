63e695ca219d39628a96e739270c6cb72492bdf6
johninamillion <johninamio@gmail.com>
2022-08-08 11:36:37 +0200

    add database credentials

--- config.php
+++ config.php
@@ -1,3 +1,11 @@
 <?php
 
 define( 'APP_ROOT_DIR',     __DIR__ );
+
+define( 'DATABASE_TYPE',    'mysql' );
+define( 'DATABASE_HOST',    'localhost' );
+define( 'DATABASE_CHARSET', 'utf8' );
+define( 'DATABASE_PORT',    '3306' );
+define( 'DATABASE_NAME',    'sae_5100_0322_api' );
+define( 'DATABASE_USER',    'root' );
+define( 'DATABASE_PASS',    'root' );
