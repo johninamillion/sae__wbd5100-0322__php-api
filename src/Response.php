5853b179ce0e0c6c68623c0a9591a9bf78b0d4d1
johninamillion <johninamio@gmail.com>
2022-08-08 11:10:05 +0200

    create final Response class

---
+++ src/Response.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SAE\PHPAPI;
+
+final class Response {
+
+    const DEFAULT_CONTENT_TYPE = 'application/json';
+    const DEFAULT_RESPONSE_CODE = 200;
+
+    /**
+     * Set content type
+     *
+     * @access  public
+     * @param   string  $content_type
+     * @return  void
+     */
+    public function setContentType( string $content_type = self::DEFAULT_CONTENT_TYPE ) : void {
+
+        header( "Content-Type: $content_type" );
+    }
+
+    /**
+     * Set HTTP response code
+     *
+     * @access  public
+     * @param   int     $code
+     * @return  int|bool
+     */
+    public function setResponseCode( int $code = self::DEFAULT_RESPONSE_CODE ) : int|bool {
+
+        return http_response_code( $code );
+    }
+
+}
