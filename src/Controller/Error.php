11e5bddb5138f0d2926568ba9cd03be2ce016932
johninamillion <johninamio@gmail.com>
2022-08-08 11:03:10 +0200

    create final Error controller

---
+++ src/Controller/Error.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SAE\PHPAPI\Controller;
+
+use SAE\PHPAPI\Controller;
+
+final class Error extends Controller {
+
+    /**
+     * Init
+     *
+     * @access  public
+     * @param   int     $status
+     * @return  void
+     */
+    public static function init( int $status = 404 ) : void {
+        ( new self() )->index( $status );
+    }
+
+    /**
+     * Index
+     *
+     * @access  public
+     * @param   int     $status
+     * @return  void
+     */
+    public function index( int $status = 404 ) : void {
+
+    }
+
+}
