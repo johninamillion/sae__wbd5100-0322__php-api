9cc0e7b8d9b0b9c760988e06e8a2803944171502
johninamillion <johninamio@gmail.com>
2022-08-08 11:19:55 +0200

    print error messages

--- src/Controller/Error.php
+++ src/Controller/Error.php
@@ -7,9 +7,52 @@ use SAE\PHPAPI\Controller;
 final class Error extends Controller {
 
     /**
+     * Get error message by status code
+     *
+     * @access  private
+     * @static
+     * @param   int     $code
+     * @return  string|NULL
+     */
+    private static function getMessage( int $code ) : ?string {
+        /** @var string $messages */
+        $messages = [
+            '400'   =>  _( "Bad Request" ),
+            '401'   =>  _( "Unauthorized" ),
+            '402'   =>  _( "Payment Required" ),
+            '403'   =>  _( "Forbidden" ),
+            '404'   =>  _( "Not Found" ),
+            '405'   =>  _( "Method Not Allowed" ),
+            '406'   =>  _( "Not Acceptable" ),
+            '407'   =>  _( "Proxy Authentication Required" ),
+            '408'   =>  _( "Request Timeout" ),
+            '409'   =>  _( "Conflict" ),
+            '410'   =>  _( "Gone" ),
+            '411'   =>  _( "Length Required" ),
+            '412'   =>  _( "Precondition Failed" ),
+            '413'   =>  _( "Payload Too Large" ),
+            '414'   =>  _( "URI Too Long" ),
+            '415'   =>  _( "Unsupported Media Type" ),
+            '416'   =>  _( "Range Not Satisfiable" ),
+            '417'   =>  _( "Expectation Failed" ),
+            '418'   =>  _( "I'm a teapot" ),
+            '422'   =>  _( "Unprocessable Entity" ),
+            '425'   =>  _( "Too Early" ),
+            '426'   =>  _( "Upgrade Required" ),
+            '428'   =>  _( "Precondition Required" ),
+            '429'   =>  _( "Too many Requests" ),
+            '431'   =>  _( "Request Header Fields Too Large" ),
+            '451'   =>  _( "Unavailable For Legal Reasons" ),
+        ];
+
+        return $messages[ $code ] ?? NULL;
+    }
+
+    /**
      * Init
      *
      * @access  public
+     * @static
      * @param   int     $status
      * @return  void
      */
@@ -25,7 +68,9 @@ final class Error extends Controller {
      * @return  void
      */
     public function index( int $status = 404 ) : void {
-        $this->Respo
+        $this->Response->setContentType( 'application/json' );
+        $this->Response->setResponseCode( $status );
+        $this->Response->printJSON( [ 'error' => [ 'code' => $status, 'message' => self::getMessage( $status ) ] ] );
     }
 
 }
