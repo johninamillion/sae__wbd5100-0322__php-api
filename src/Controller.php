7d847081e5c1a5b687e5ddcc420ae51151469624
johninamillion <johninamio@gmail.com>
2022-08-08 11:11:23 +0200

    create an instance of Response

--- src/Controller.php
+++ src/Controller.php
@@ -4,4 +4,22 @@ namespace SAE\PHPAPI;
 
 abstract class Controller {
 
+    /**
+     * Response
+     *
+     * @access  private
+     * @var     Response|NULL
+     */
+    private ?Response $Response = NULL;
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @construct
+     */
+    public function __construct() {
+        $this->Response = new Response();
+    }
+
 }
