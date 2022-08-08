44e1d440e70916d5b325080a1bebb0a9df038b0c
johninamillion <johninamio@gmail.com>
2022-08-08 12:14:44 +0200

    add getter and isset for errors

--- src/Model.php
+++ src/Model.php
@@ -62,4 +62,32 @@ abstract class Model {
         $this->messages[ $key ][] = $message;
     }
 
+    /**
+     * Get errors
+     *
+     * @access  public
+     * @param   string|NULL $key
+     * @return  array
+     */
+    public function getErrors( ?string $key = NULL ) : array {
+
+        return is_null( $key )
+            ? $this->errors
+            : $this->errors[ $key ];
+    }
+
+    /**
+     * Has errors
+     *
+     * @access  public
+     * @param   string|NULL $key
+     * @return  bool
+     */
+    public function hasErrors( ?string $key = NULL ) : bool {
+
+        return is_null( $key )
+            ? isset( $this->errors ) && !empty( $this->errors )
+            : isset( $this->errors[ $key ] ) && !empty( $this->errors[ $key ] );
+    }
+
 }
